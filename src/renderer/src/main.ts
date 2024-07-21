4d5cf91fbd6bc94dc3b652e114722998e47ca790
wucunqian <2801531707@qq.com>
2024-07-21 16:49:10 +0800

    feat：集成插件配置

--- src/renderer/src/main.ts
+++ src/renderer/src/main.ts
@@ -1,6 +1,9 @@
-import './assets/main.css'
+import { createApp } from 'vue';
+import App from './App.vue';
+import router from './router';
+import { createPinia } from 'pinia';
+import ElementPlus from 'element-plus';
+import 'element-plus/dist/index.css';
+import './assets/main.css';
 
-import { createApp } from 'vue'
-import App from './App.vue'
-
-createApp(App).mount('#app')
+createApp(App).use(router).use(createPinia()).use(ElementPlus).mount('#app');
---
+++ src/renderer/src/views/login/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    login {{ stores.objGather.title }} <br>
+    <div class="mb-4">
+      <el-button>Default</el-button>
+      <el-button type="primary">Primary</el-button>
+      <el-button type="success">Success</el-button>
+      <el-button type="info">Info</el-button>
+      <el-button type="warning">Warning</el-button>
+      <el-button type="danger">Danger</el-button>
+    </div>
+  </div>
+
+</template>
+
+<script setup lang="ts">
+import { useLoginStore } from '../../stores/useLogin';
+const stores = useLoginStore();
+console.log('stores')
+
+</script>
+
+<style scoped></style>
